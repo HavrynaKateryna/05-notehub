b0d0b3d53997c2d31d8374817f6c369285e77249
Kateryna Havryna <khavryna@gmail.com>
2026-02-21 23:11:38 +0100

    HW-5

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,13 @@
+import toast from "react-hot-toast";
+
+export default function Error() {
+  return (
+    <>
+      <p>
+        {toast.error(
+          "Sorry! Something went wrong",
+        )}
+      </p>
+    </>
+  );
+}
